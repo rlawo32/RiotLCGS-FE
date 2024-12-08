7391386b3000f2af683fab11be39bdc4e08a99ba
rlawo <rlawo32@naver.com>
2024-12-08 22:57:00 +0900

    [RiotLCGS][FE] <통신> RiotClient_연결_작업_20241208 (1)

---
+++ src/app/api/RiotWebSocket.ts
@@ -0,0 +1,39 @@
+import { authenticate, createHttp1Request } from "league-connect";
+
+const RiotWebSocket = async(gameId:number) => {
+
+  // console.log(credentials);
+  
+  console.log(gameId);
+
+  const credentials = await authenticate({
+    awaitConnection: true,
+    pollInterval: 1000,
+  });
+  console.log(credentials);
+  /*
+  const cwsc = await createWebSocketConnection({
+    authenticationOptions: {
+      awaitConnection: true,
+    },
+    pollInterval: 1000, // wait time
+    maxRetries: 10      // retry count
+  });
+  */
+  
+  const response = await createHttp1Request({
+    method: 'GET',
+    url: '/lol-match-history/v1/games/' + gameId
+  }, credentials);
+
+  console.log(response.json());
+
+  return JSON.stringify(response.json());
+  /*
+  cwsc.subscribe("/lol-store/v1/catalog/sales", (data) => {
+    console.log(data);
+  });
+  */
+}
+
+export default RiotWebSocket;
