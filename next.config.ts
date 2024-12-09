e4c3578041c0e632745dff307052353f0e21b5c0
rlawo <rlawo32@naver.com>
2024-12-09 22:55:17 +0900

    [RiotLCGS][FE] <통신> Proxy_작업_20241209 (4)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `http://localhost:8080/:path*`
+      },
+    ];
+  },
 };
 
 export default nextConfig;
