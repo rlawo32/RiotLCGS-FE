343e37b822970e3a0a8cd1f3194a09e275360469
rlawo <rlawo32@naver.com>
2024-12-25 03:10:42 +0900

    [RiotLCGS][FE] <View> LOL프로세스_확인_작업_20241225 (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import MainView from "./view/MainView";
+import MainTask from "./view/MainTask";
 import styles from "./page.module.css";
 
-import RiotClient from "@/app/api/RiotWebSocket";
-
 export default async function Home() {
-  const gameId:number = 7389173588;
-  const gameData = await RiotClient(gameId);
   
   return (
     <div className={styles.page}>
-      <MainView gameId={gameId} gameData={gameData} />
+      <MainTask />
     </div>
   );
 }
